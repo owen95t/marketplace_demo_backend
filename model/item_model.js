c22ef5e9531980f24094a9adf863bafdd9076cd2
owentangjintanakarn <owen.t95@hotmail.com>
2021-11-09 14:37:52 +0700

    item model implemented user model implemented

--- model/item_model.js
+++ model/item_model.js
@@ -1,8 +1,44 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
+
+// const user = require('../model/user_model')
 
 let item_model = new Schema({
-
+    item_name: {
+        type: String,
+        required: true
+    },
+    item_desc: {
+        type: String,
+        required: true
+    },
+    item_price: {
+        type: Number,
+        required: true
+    },
+    item_discount:{
+        type: Number,
+        required: true
+    },
+    item_owner_id: {
+        type: String,
+        required: true
+    },
+    date_created: {
+        type: Date,
+        required: true
+    },
+    item_status: {
+        type: Boolean,
+        required: true
+    },
+    item_rating: {
+        type: Number,
+        min: 0,
+        max: 5
+    }
+}, {
+    collection: 'items'
 })
 
 module.exports = item_model
---
+++ model/user_model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now()
+    },
+    role: {
+        type: String,
+        default: 'user' //two options: user or admin.
+    }
+}, {
+    collection: 'users'
+})
+
+module.exports = mongoose.model("UserSchema", UserSchema)
