381fada985ab4c7184d6371732f8a73bd3a8f397
owentangjintanakarn <owen.t95@hotmail.com>
2021-11-14 14:31:19 +0700

    minor changes to files pt.3

--- routes/index_router.js
+++ routes/index_router.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res) => {
+  res.status(200).json({message: 'You are at the root of the api'})
+})
 
 module.exports = router;
